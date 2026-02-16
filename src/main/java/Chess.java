3cedea29aed0f9a414cfd11e14cf900d92b06ba1
Mou Balsera <moubalsera@gmail.com>
2026-02-16 21:27:30 +0100

    Errors in maven project creation corrected

--- src/main/java/Chess.java
+++
@@ -1,8 +0,0 @@
-public class Chess {
-     public static void main(String[] args){
-      
-       Game game = new Game();
-       game.play();
-
-    }      
-   }
---
+++ src/main/java/com/chessgame/Chess.java
@@ -0,0 +1,8 @@
+package src.main.java.com.chessgame;
+
+public class Chess {
+	public static void main(String[] args){
+		Game game = new Game();
+		game.play();
+	}
+}
